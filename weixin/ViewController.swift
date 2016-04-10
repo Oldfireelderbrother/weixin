7d6dfe008d6f079d3185886602d53462d2abd372
Oldfireelderbrother <61824943@qq.com>
2016-04-10 10:15:19 +0800

    Initial Commit

---
+++ weixin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weixin
+//
+//  Created by 博 郑 on 16/4/10.
+//  Copyright © 2016年 博 郑&&书潇 黄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
